cc8a7e378e257d23a86dafd63779e63a87fe3f69
nitsuga5124 <nitsuga5124@gmail.com>
2020-02-04 02:54:23 +0100

    Moved capitalize_first function to a utilities module 	new file:   utils/basic_functions.rs 	new file:   utils/mod.rs

---
+++ src/utils/basic_functions.rs
@@ -0,0 +1,8 @@
+/// Capitalizes the first letter of a str.
+pub fn capitalize_first(input: &str) -> String {
+    let mut c = input.chars();
+    match c.next() {
+        None => String::new(),
+        Some(first) => first.to_uppercase().collect::<String>() + c.as_str(),
+    }
+}
