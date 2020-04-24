ae70a9aee0622ae01c2175f0f72d617063d548fb
nitsuga5124 <nitsuga5124@gmail.com>
2020-04-24 13:55:00 +0200

    Added pi recognition, anti nsfw jumps and fefer easter egg. 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -109,7 +109,10 @@ use serenity::{
             Activity,
         },
         user::OnlineStatus,
-        id::UserId,
+        id::{
+            UserId,
+            ChannelId,
+        },
     },
     prelude::{
         EventHandler,
@@ -411,6 +414,54 @@ impl EventHandler for Handler {
             } else if msg.content == "ayy" {
                 let _ = msg.channel_id.say(&ctx, "lmao").await; // say just send the message
 
+            }
+        }
+        if msg.content.contains("discordapp.com/channels/") {
+            let mut splits = msg.content.split('/');
+            if splits.clone().count() == 7 {
+                let channel_id  = splits.nth(5).unwrap_or("0").parse::<u64>().expect("NaN");
+                if let Ok(chan) = ChannelId(channel_id).to_channel(&ctx).await {
+                    if chan.is_nsfw().await {
+                        let _ = msg.react(&ctx, "🇳").await;
+                        let _ = msg.react(&ctx, "🇸").await;
+                        let _ = msg.react(&ctx, "🇫").await;
+                        let _ = msg.react(&ctx, "🇼").await;
+                    }
+                }
+            }
+        }
+
+        if msg.content.starts_with("3.14") || msg.content.starts_with("3,14") {
+            let content = msg.content.replace(",", ".");
+            let pif = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491298336733624406566430860213949463952247371907021798609437027705392171762931767523846748184676694051320005681271452635608277857713427577896091736371787214684409012249534301465495853710507922796892589235420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185950244594553469083026425223082533446850352619311881710100031378387528865875332083814206171776691473035982534904287554687311595628638823537875937519577818577805321712268066130019278766111959092164201989";
+
+            let l = if pif.len() > content.len() {
+                content.len()
+            } else {
+                pif.len()
+            };
+
+
+            let mut correct = true;
+
+            for i in 0..l {
+                if pif.chars().into_iter().nth(i) != content.chars().into_iter().nth(i) {
+                    correct = false;
+                }
+            }
+
+            if correct {
+                let _ = msg.react(&ctx, "✅").await;
+            } else {
+                let _ = msg.react(&ctx, "❌").await;
+            }
+        }
+
+        if msg.guild_id.unwrap_or_default().0 == 159686161219059712 {
+            if msg.content.contains("ping me on nsfw!") {
+                let _ = ChannelId(354294536198946817).say(&ctx, format!("<@{}>", msg.author.id)).await;
+            } else if msg.content.contains("ping him on nsfw!") {
+                let _ = ChannelId(354294536198946817).say(&ctx, "<@299624139633721345>").await;
             }
         }
     }
