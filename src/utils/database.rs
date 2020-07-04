46f52590fd529f4b7c06b1d66e589b0889edb1d5
nitsuga5124 <nitsuga5124@gmail.com>
2020-07-04 16:55:16 +0200

    repaced compile time macro with runtime method. 	modified:   src/utils/database.rs

--- src/utils/database.rs
+++ src/utils/database.rs
@@ -1,9 +1,10 @@
+use std::env;
 use sqlx::postgres::PgPool;
 
 // This function obtains a database connection to the postgresql database used for the bot.
 pub async fn obtain_pool() -> Result<PgPool, Box<dyn std::error::Error>> {
     // Obtain the postgresql url.
-    let pg_url = env!("DATABASE_URL2");
+    let pg_url = env::var("DATABASE_URL2")?;
 
     // Connect to the database with the information provided on the configuration.
     // and return a pool of connections
