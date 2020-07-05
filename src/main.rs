eb0f936a2e742fff1761201c83fe0ad50f69986d
nitsuga5124 <nitsuga5124@gmail.com>
2020-07-05 13:20:56 +0200

    Moded raw events to it's own file. 	new file:   src/logging.rs 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@
 mod utils; // Load the utils module
 mod commands; // Load the commands module
 mod notifications;
+mod logging;
 
 // Import this 2 commands in specific with a different name
 // as they interfere with the configuration commands that are also being imported.
@@ -42,7 +43,7 @@ use utils::basic_functions::capitalize_first; // Obtain the capitalize_first fun
 use std::{
     collections::{
         HashSet, // Low cost indexable lists.
-        HashMap,
+        //HashMap,
     },
     // For saving / reading files
     fs::File,
@@ -67,7 +68,7 @@ use tracing::{
     // Log macros.
     info,
     trace,
-    warn,
+    //warn,
     error,
     // Others
     Level,
@@ -132,15 +133,12 @@ use serenity::{
             UserId,
             ChannelId,
             GuildId,
-            MessageId,
         },
         event::VoiceServerUpdateEvent,
         guild::Member,
-        event::Event,
     },
     prelude::{
         EventHandler,
-        RawEventHandler,
         Context,
         TypeMapKey,
         RwLock,
@@ -401,90 +399,6 @@ async fn my_help(
     help_commands::with_embeds(ctx, msg, args, &ho, groups, owners).await
 }
 
-
-struct RawHandler; // Defines the raw handler to be used for logging.
-
-#[async_trait]
-impl RawEventHandler for RawHandler {
-    async fn raw_event(&self, ctx: Context, event: Event) {
-        let data_read = ctx.data.read().await;
-        let redis_pool = data_read.get::<RedisPool>().unwrap();
-        let mut redis = redis_pool.get().await;
-
-        match event {
-            Event::MessageCreate(data) => {
-                let message = data.message;
-
-                if message.author.bot || message.guild_id.is_none() {
-                    return;
-                }
-
-                let rdata = ctx.data.read().await;
-                let pool = rdata.get::<ConnectionPool>().unwrap();
-
-                let data = match sqlx::query!("SELECT enabled FROM anti_spam WHERE guild_id = $1", message.guild_id.unwrap().0 as i64)
-                    .fetch_optional(pool)
-                    .await {
-                        Err(why) => {
-                            error!("Error quering database for anti_spam: {}", why);
-                            return;
-                        },
-                        Ok(x) => x
-                };
-
-                if let Some(row) = data {
-                    if row.enabled {
-                        if let Err(why) = redis.append(message.author.id.0.to_string(), format!("{}|{},", message.id.0, message.channel_id.0)).await {
-                            error!("Error sending data to redis: {}", why);
-                        }
-
-                        if let Err(why) = redis.expire_seconds(message.author.id.0.to_string(), 5).await {
-                            error!("Error setting expire date to redis: {}", why);
-                        }
-
-                        match redis.get(message.author.id.0.to_string()).await {
-                            Err(why) => error!("Error getting message data from redis: {}", why),
-                            Ok(x) => {
-                                if let Some(messages) = x {
-                                    let mut messages_channels = String::from_utf8(messages).unwrap();
-                                    messages_channels.pop();
-                                    let messages = messages_channels.split(',');
-
-                                    if messages.clone().count() > 5 {
-                                        let mut bad_messages: HashMap<u64, Vec<MessageId>> = HashMap::new();
-
-                                        for msg_chan in messages {
-                                            let mut split = msg_chan.split('|');
-                                            let message_id = split.nth(0).unwrap().parse::<u64>().unwrap();
-                                            let channel_id = split.nth(0).unwrap().parse::<u64>().unwrap();
-
-                                            if let Some(x) = bad_messages.get_mut(&channel_id) {
-                                                x.push(MessageId(message_id));
-                                            } else {
-                                                bad_messages.insert(channel_id, vec![MessageId(message_id)]);
-                                            }
-                                        }
-
-                                        for (channel, message_ids) in bad_messages.iter() {
-                                            let _ = ChannelId(*channel).delete_messages(&ctx, message_ids).await;
-                                        }
-
-                                        let _ = message.reply(&ctx, "No spamming.").await;
-                                        let _ = redis.del(message.author.id.0.to_string()).await;
-                                    }
-                                } else {
-                                    warn!("This should never happen! Redis didn't obtain the message data that just got sent.");
-                                }
-                            }
-                        }
-                    }
-                }
-            },
-
-            _ => (),
-        }
-    }
-}
 struct Handler; // Defines the handler to be used for events.
 
 #[async_trait]
@@ -1041,7 +955,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut client = Client::new(&bot_token)
         .event_handler(Handler)
-        .raw_event_handler(RawHandler)
+        .raw_event_handler(logging::RawHandler)
         .framework(std_framework)
         .add_intent({
             let mut intents = GatewayIntents::all();
